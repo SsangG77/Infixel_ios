1725b874bdb9d0dd46cfafa294b9d4fda18ca21e
[sangjin] <sangjincha719@gmail.com>
2023-09-12 20:53:40 +0900

    Initial Commit

---
+++ Infixel/InfixelApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfixelApp.swift
+//  Infixel
+//
+//  Created by 차상진 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct InfixelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
