0f038c5396f8601dacba2fefe612c84428f5433b
sangjin <sangjincha719@gmail.com>
2024-06-26 22:52:03 +0900

    이미지 스크롤뷰 새로 생성

---
+++ Infixel/PageGroups/ViewModel/ImageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ImageViewModel.swift
+//  Infixel
+//
+//  Created by 차상진 on 6/22/24.
+//
+
+import Foundation
---
+++ Infixel/Test/ScrollVIew_test.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollVIew_test.swift
+//  Infixel
+//
+//  Created by 차상진 on 6/25/24.
+//
+
+import SwiftUI
+
+struct ScrollVIew_test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollVIew_test()
+}
