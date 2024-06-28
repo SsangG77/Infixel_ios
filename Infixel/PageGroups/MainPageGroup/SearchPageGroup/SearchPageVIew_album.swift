11fd617dc782a27c117085b56ce90fcac85a7462
sangjin <sangjincha719@gmail.com>
2024-06-29 02:17:52 +0900

    검색 페이지 부분에 검색 결과 구분하는 탭 버튼 만듬

---
+++ Infixel/PageGroups/MainPageGroup/SearchPageGroup/SearchPageVIew_album.swift
@@ -0,0 +1,18 @@
+//
+//  SearchPageVIew_album.swift
+//  Infixel
+//
+//  Created by 차상진 on 6/29/24.
+//
+
+import SwiftUI
+
+struct SearchPageVIew_album: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchPageVIew_album()
+}
---
+++ Infixel/PageGroups/MainPageGroup/SearchPageGroup/SearchPageView_user.swift
@@ -0,0 +1,18 @@
+//
+//  SearchPageView_user.swift
+//  Infixel
+//
+//  Created by 차상진 on 6/28/24.
+//
+
+import SwiftUI
+
+struct SearchPageView_user: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchPageView_user()
+}
