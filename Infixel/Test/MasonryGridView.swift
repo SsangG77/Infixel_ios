adc713c48ce2daa970f812e18f8ce5a75e59e9b9
sangjin <sangjincha719@gmail.com>
2024-07-07 00:24:17 +0900

    검색 페이지 완료

--- Infixel/Test/MasonryGridView.swift
+++ Infixel/Test/MasonryGridView.swift
@@ -1,18 +1,18 @@
 //
-//  UICoordinator.swift
+//  MasonryGridView.swift
 //  Infixel
 //
-//  Created by 차상진 on 7/3/24.
+//  Created by 차상진 on 7/7/24.
 //
 
 import SwiftUI
 
-struct UICoordinator: View {
+struct MasonryGridView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    UICoordinator()
+    MasonryGridView()
 }
--- Infixel/Test/PinterestView_test/Model/Item.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Item.swift
-//  Infixel
-//
-//  Created by 차상진 on 7/2/24.
-//
-
-import Foundation
